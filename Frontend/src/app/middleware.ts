b7fa2b4a2de8b4a3ad65b244dde9cdab7bb453b8
Yashk0809 <yashk5316@gmail\.com>
2025-06-23 22:29:54 +0530

    updated Protected routes

--- Frontend/src/app/middleware.ts
+++ Frontend/src/app/middleware.ts
@@ -6,7 +6,9 @@ export function middleware(request: NextRequest) {
   const token = request.cookies.get('token')?.value;
 
   // Redirect unknown users to /login if trying to access /dashboard pages
-  if (pathname.startsWith('/dashboard') && !token) {
+  const protectedRoutes = ['/dashboard', '/upload-csv', '/add'];
+
+  if (protectedRoutes.some(route => pathname.startsWith(route)) && !token) {
     const loginUrl = new URL('/login', request.nextUrl.origin);
     return NextResponse.redirect(loginUrl);
   }
