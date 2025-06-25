2d6226c0da5a263d851a592582b7781a1a7fca7e
Yashk0809 <yashk5316@gmail\.com>
2025-06-25 13:35:52 +0530

    logout fix

--- Backend/src/controllers/authController.ts
+++ Backend/src/controllers/authController.ts
@@ -53,7 +53,9 @@ export const logout = async (req: Request, res: Response) => {
   res.clearCookie('token', {
     httpOnly: true,
     secure: process.env.NODE_ENV === 'production',        
-    sameSite: 'lax',
+    sameSite: 'none',
+    domain: '.yash-admin-0823842.fwh.is',
+    path: '/',
   });
   return res.status(200).json({ message: 'Logged out successfully' });
 };
